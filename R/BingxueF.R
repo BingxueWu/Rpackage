d1abc4afc34f17855fe881713f6d04e08162e0a8
BingxueWu <bingxuewu07@gmail.com>
2026-04-16 15:08:23 +0200

    test

---
+++ R/BingxueF.R
@@ -0,0 +1,20 @@
+#' Bingxue
+#'
+#'Practice creating R packages
+#'
+#' @author Bingxue Wu
+#'
+#' @param x a numeric value
+#'
+#'
+#' @returns  the square of x
+#'
+#' @export
+#'
+#' @examples BingxueF(3)
+#'
+BingxueF <- function(x){
+    y = x * x
+    return(y)
+}
+BingxueF(3)
